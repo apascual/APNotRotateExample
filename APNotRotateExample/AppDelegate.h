5f7321b6ce6486595b46a78f36da2cbc017796ff
Abel Pascual <abelpascual@gmail.com>
2015-03-24 10:50:47 +0100

    Initial Commit

---
+++ APNotRotateExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APNotRotateExample
+//
+//  Created by Abel Pascual on 24/03/15.
+//  Copyright (c) 2015 Abel Pascual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APNotRotateExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APNotRotateExample
+//
+//  Created by Abel Pascual on 24/03/15.
+//  Copyright (c) 2015 Abel Pascual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
